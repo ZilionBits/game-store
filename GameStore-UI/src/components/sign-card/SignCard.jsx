c470fd48240b3b18c75ba8c86e944ac1dad6b885
Darius Zilionis <Darius.Zilionis@aol.com>
2025-01-21 07:55:54 +0200

    basic signin form done

---
+++ GameStore-UI/src/components/sign-card/SignCard.jsx
@@ -0,0 +1,15 @@
+import { Stack, styled } from '@mui/material';
+
+export const SignCard = styled(Stack)(({ theme }) => ({
+  position: 'relative',
+  width: '350px',
+  height: '600px',
+  margin: 'auto',
+  top: '10vh',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'space-evenly',
+  boxShadow: theme.shadows[5],
+  backgroundColor: 'rgba(255, 255, 255, 0.1)',
+}));
--- GameStore-UI/src/components/sign-in/SignIn.jsx
+++ GameStore-UI/src/components/sign-in/SignIn.jsx
@@ -1,29 +1,101 @@
-import { Stack, styled, Typography } from '@mui/material';
+import { Typography, InputLabel, Input, Button, Divider, FormHelperText, FormControl } from '@mui/material';
+import { Link } from 'react-router';
+import { SignCard } from '../sign-card/SignCard';
+import { useState } from 'react';
 
 export const SignIn = () => {
-  const SignInCard = styled(Stack)(({ theme }) => ({
-    position: 'relative',
-    width: '400px',
-    height: '600px',
-    margin: 'auto',
-    top: '10vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-    boxShadow: theme.shadows[5],
-    backgroundColor: 'rgba(255, 255, 255, 0.1)',
-  }));
+  const [signInForm, setSignInForm] = useState({
+    username: '',
+    password: '',
+  });
+  const [signInFormError, setSignInFormError] = useState({
+    username: false,
+    usernameErrorMessage: ' ',
+    password: false,
+    passwordErrorMessage: ' ',
+  });
+
+  const inputValidation = (key, value) => {
+    let isValid = false;
+    
+    if (key === 'username') {
+      isValid = value.length >= 3;
+      if (isValid) {
+        setSignInFormError((error) => ({ ...error, usernameErrorMessage: ' ' }));
+      }
+    }
+    if (key === 'password') {
+      isValid = value.length >= 6;
+      if (isValid) {
+        setSignInFormError((error) => ({ ...error, passwordErrorMessage: ' ' }));
+      }
+    }
+    return isValid;
+  };
+
+  const handleChange = (e) => {
+    const key = e.target.name;
+    const value = e.target.value;
+    setSignInForm((form) => ({ ...form, [key]: value }));
+    setSignInFormError((error) => ({ ...error, [key]: !inputValidation(key, value) }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (signInForm.username.length < 3 || signInForm.password.length < 6) {
+      if (signInForm.username.length < 3) {
+        setSignInFormError((error) => ({ ...error, usernameErrorMessage: 'Username must be at least 3 characters.' }));
+      }
+      if (signInForm.password.length < 6) {
+        setSignInFormError((error) => ({ ...error, passwordErrorMessage: 'Password must be at least 6 characters.' }));
+      }
+      return;
+    }
+
+    console.log(signInForm);
+  };
 
   return (
-    <>
-      <SignInCard>
-        <Typography variant='h2' color='primary'>SIGN IN</Typography>
-        <Typography>Hello</Typography>
-        <Typography>Hello</Typography>
-        <Typography>Hello</Typography>
-        <Typography>Hello</Typography>
-      </SignInCard>
-    </>
+    <SignCard>
+      <Typography variant="h3" color="primary" letterSpacing={10}>
+        SIGN IN
+      </Typography>
+      <form noValidate style={{ display: 'flex', flexDirection: 'column', gap: '1.5rem', width: '80%' }}>
+        <FormControl>
+          <InputLabel htmlFor="username">Username</InputLabel>
+          <Input
+            id="username"
+            name="username"
+            type="text"
+            value={signInForm.username}
+            onChange={handleChange}
+            autoFocus
+          />
+          <FormHelperText error={signInFormError.username}>{signInFormError.usernameErrorMessage}</FormHelperText>
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="password">Password</InputLabel>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            value={signInForm.password}
+            onChange={handleChange}
+            autoComplete="off"
+          />
+          <FormHelperText error={signInFormError.password}>{signInFormError.passwordErrorMessage}</FormHelperText>
+        </FormControl>
+        <Button variant='outlined' onClick={handleSubmit} type="submit">
+          Sign In
+        </Button>
+      </form>
+      <Divider flexItem variant="middle">
+        or
+      </Divider>
+      <Typography>
+        Create new account <Link to={'/signup'}>Sign Up</Link>.
+      </Typography>
+    </SignCard>
   );
 };
