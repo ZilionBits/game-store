44739e36a2c36eeb39d237550cdc3aea94cd5467
Darius Zilionis <Darius.Zilionis@aol.com>
2025-01-25 13:48:47 +0200

    homepage design improvement

--- GameStore-UI/src/components/product-list-page/ProductList.jsx
+++ GameStore-UI/src/components/product-list-page/ProductList.jsx
@@ -1,6 +1,6 @@
 import { ProductCard } from './ProductCard';
 import storeData from '../../temp/game_data.json';
-import { Box, Grid2 } from '@mui/material';
+import { Box, Container, Grid2 } from '@mui/material';
 
 export const ProductListPage = () => {
   console.log(storeData);
@@ -8,19 +8,26 @@ export const ProductListPage = () => {
   return (
     <>
       <h1>Store List</h1>
-      <Grid2 container spacing={2} justifyContent={'space-evenly'}>
-        {storeData.map((data, index) => (
-          <ProductCard
-            key={index}
-            name={data.name}
-            platforms={data.platforms.join(' ')}
-            metascore={data.metascore}
-            image_url={data.image_url}
-            price={data.price}
-            genres={data.genres.join(' ')}
-          />
-        ))}
-      </Grid2>
+      <Container fixed>
+        <Grid2 container spacing={2}>
+          {storeData.map((data) => (
+            <Grid2
+              key={data.name}
+              size={{ xs: 12, sm: 6, md: 4, lg: 3 }}
+              sx={{ display: 'flex', justifyContent: 'center' }}
+            >
+              <ProductCard
+                name={data.name}
+                platforms={data.platforms.join(' ')}
+                metascore={data.metascore}
+                image_url={data.image_url}
+                price={data.price}
+                genres={data.genres.join(' ')}
+              />
+            </Grid2>
+          ))}
+        </Grid2>
+      </Container>
     </>
   );
 };
