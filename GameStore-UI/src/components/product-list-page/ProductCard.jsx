f76132661e1224075cba15aabf6230a5f42111c2
Darius Zilionis <Darius.Zilionis@aol.com>
2025-01-29 12:17:36 +0200

    change price show logic in UI

--- GameStore-UI/src/components/product-list-page/ProductCard.jsx
+++ GameStore-UI/src/components/product-list-page/ProductCard.jsx
@@ -1,21 +1,30 @@
-import { Box, Card, CardActionArea, CardActions, CardContent, CardHeader, CardMedia, Typography, Button } from '@mui/material';
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Typography,
+  Button,
+} from '@mui/material';
 import { Link } from 'react-router';
 
 export const ProductCard = (props) => {
   const { name, platforms, metascore, image_url, price, genres } = props;
 
   return (
-    <Card sx={{width:'250px'}}>
+    <Card sx={{ width: '250px' }}>
       <CardActionArea>
-        <CardMedia image={image_url} sx={{ height: '200px'}} />
+        <CardMedia image={image_url} sx={{ height: '200px' }} />
         <CardContent sx={{ padding: '0px' }}>
           <CardHeader title={name} subheader={platforms} avatar={metascore} sx={{ padding: '5px 0px 5px 0px' }} />
           <Typography component={Box} variant="caption" sx={{ marginLeft: '8px' }}>
             Genre: {genres}
           </Typography>
           <Typography component={Box} sx={{ textAlign: 'end', marginRight: '8px' }}>
-            {price}
-            {price !== 'Free-to-play' && ' €'}
+            {price !== 0 ? `${price} €` : 'Free-to-play'}
           </Typography>
         </CardContent>
       </CardActionArea>
